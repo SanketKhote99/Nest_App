16e23066b4859570bc0a337d9ab84e7f524444a1
Sanket Khote <sanketkhote43@gmail.com>
2023-02-08 11:39:19 +0530

    bookmark add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, BookmarkModule],
   controllers: [AppController],
   providers: [AppService],
 })
